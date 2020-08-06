ca2581aad69fe57d9edd04449e7849853f1a84a1
xmetal <Xmetalfanx@yahoo.com>
2020-08-06 14:06:16 -0400

    Intital Commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/universalCSS/style.css" >
+
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="headerTitle">Title Here </div>
+            <nav>Nav Here </nav>    
+        </header>
+        
+        <div class="mainSection">
+            <sidebar>Sidebar</sidebar>
+            <section>main section</section>
+        </div>
+        
+        <footer>footer</footer>
+
+    </div>
+    
+</body>
+</html>
