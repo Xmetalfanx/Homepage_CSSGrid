60068de97f34b5aa6763f10ed944f764f5f41e21
Xmetalfanx <Xmetalfanx@yahoo.com>
2020-09-18 16:39:32 -0400

    Trying to simplify CSSGrid

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php include ('templates/baseTemplateTop.php'); ?>
-<div class="middle-container">
   <aside>Sidebar</aside>
   <main>main section</main>
-</div>
 <?php include ('modules/footer.php'); ?>
